dc51a0366725cd25f090eab24453b73510a46160
bbb907 <logtoken2@gmail.com>
2026-08-17 05:03:27 +0100

    Update btoolsUtil.lua

--- Util/btoolsUtil.lua
+++ Util/btoolsUtil.lua
@@ -40,4 +40,79 @@ function btools:DestroyObject(obj)
 	return false
 end
 
+function btools:SetCollision(obj, state)
+	local remote = fetchRemote()
+
+	if remote then
+		remote:InvokeServer(table.unpack({
+			[1] = "SyncCollision",
+			[2] = {
+				[1] = {
+					["Part"] = obj,
+					["CanCollide"] = state,
+				},
+			},
+		}))
+		return true
+	end
+	
+	return false
+end
+
+function btools:SetCFrame(obj, cf)
+	local remote = fetchRemote()
+
+	if remote then
+		remote:InvokeServer("SyncMove",{
+			{
+				["Part"] = obj,
+				["CFrame"] = cf,
+			}
+		})
+		return true
+	end
+
+	return false
+end
+
+function btools:SetManyCollision(objs, state)
+	local remote = fetchRemote()
+
+	local arg = {}
+
+	for _,v in pairs(objs) do
+		table.insert(arg,{
+			["Part"] = v,
+			["CanCollide"] = state,
+		})
+	end
+
+	if remote then
+		remote:InvokeServer("SyncCollision",arg)
+		return true
+	end
+
+	return false
+end
+
+function btools:SetManyCFrame(objs, cf)
+	local remote = fetchRemote()
+	
+	local arg = {}
+	
+	for _,v in pairs(objs) do
+		table.insert(arg,{
+			["Part"] = v,
+			["CFrame"] = cf,
+		})
+	end
+	
+	if remote then
+		remote:InvokeServer("SyncMove",arg)
+		return true
+	end
+
+	return false
+end
+
 return btools
