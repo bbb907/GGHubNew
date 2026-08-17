299e0ff81b273ccb0592a381fd59d6df9b79b772
bbb907 <logtoken2@gmail.com>
2026-08-17 18:54:06 +0100

    Update savingUtil.lua

--- Util/savingUtil.lua
+++ Util/savingUtil.lua
@@ -5,26 +5,26 @@ local httpService = game:GetService("HttpService")
 local folderPath
 
 function saving:init(fp)
-	
+
 	if not writefile or not listfiles or not readfile or not makefolder or not isfolder or not isfile then
-		warn("SavingUtil: Your bad executor has like 0 unc, get a new one.")
+		warn("SavingUtil: Your executor does not support standard UNC filesystem functions.")
 		return
 	end
-	
+
 	folderPath = fp
-	
+
 	if isfolder(folderPath) then
 		saving:LoadAllProfiles()
 		return
 	end
-	
+
 	makefolder(folderPath)
-	
+
 end
 
-function getFuncs(name)
+local function getFuncs(name)
 	local funcs = {}
-	local saving = false
+	local isSaving = false
 
 	function funcs:Get()
 		return data_profiles[name]
@@ -35,13 +35,13 @@ function getFuncs(name)
 	end
 
 	function funcs:Save()
-		if saving then return end
-		
-		saving = true
-		
-		task.delay(2,function()
-			writefile(folderPath.."/"..name,httpService:JSONEncode(data_profiles[name]))
-			saving = false
+		if isSaving then return end
+
+		isSaving = true
+
+		task.delay(2, function()
+			writefile(folderPath.."/"..name, httpService:JSONEncode(data_profiles[name]))
+			isSaving = false
 		end)
 	end
 
@@ -49,48 +49,54 @@ function getFuncs(name)
 end
 
 function saving:CreateProfile(name)
-	
 	local profile = data_profiles[name]
-	
+
 	if profile then
 		warn("Profile already exists!")
 		return saving:GetProfile(name)
 	end
-	
+
 	data_profiles[name] = {}
-	
+
 	return getFuncs(name)
-	
 end
 
 function saving:GetProfile(name)
 	local profile = data_profiles[name]
-	
+
 	if not profile then
 		warn("Profile does not exist!")
 		return
 	end
 
-	data_profiles[name] = {}
-
 	return getFuncs(name)
 end
 
 function saving:LoadProfile(path)
-	local s,e = pcall(function()
-		data_profiles[string.split(path,"\\")[2]] = httpService:JSONDecode(readfile(path))
+	local s, e = pcall(function()
+		local content = readfile(path)
+
+		local fileName = string.match(path, "([^/]+)$") 
+
+		if fileName then
+			data_profiles[fileName] = httpService:JSONDecode(content)
+		end
 	end)
+
+	if not s then
+		warn("Failed to load profile (".. tostring(path) .."): " .. tostring(e))
+	end
 end
 
 function saving:LoadAllProfiles()
-	for _,v in pairs(listfiles(folderPath)) do
-		v = string.gsub(v,"\\","/")
-		
+	for _, v in pairs(listfiles(folderPath)) do
+		v = string.gsub(v, "\\", "/")
+
 		if not isfile(v) then
-			warn("Invalid file path: "..v)
+			warn("Invalid file path: " .. v)
 			continue
 		end
-		
+
 		saving:LoadProfile(v)
 	end
 end
