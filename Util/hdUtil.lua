f0de29c0114fe83b4a59b750a04ca1aaef8d2762
bbb907 <logtoken2@gmail.com>
2026-08-17 20:45:44 +0100

    Update hdUtil.lua

--- Util/hdUtil.lua
+++ Util/hdUtil.lua
@@ -1,15 +1,20 @@
-local hdUtil = {}
+local hdUtil = {
+	HDAdminEnabled = false
+}
 
 local signals = game:GetService("ReplicatedStorage"):FindFirstChild("HDAdminHDClient")
 
 if signals then
 	signals = signals.Signals
-else
-	return nil
+	hdUtil.HDAdminEnabled = true
 end
 
 function hdUtil:SendCommand(cmd)
+	if not hdUtil.HDAdminEnabled then return false end
+	
 	signals.RequestCommandSilent:InvokeServer(cmd)
+	
+	return true
 end
 
 return hdUtil
