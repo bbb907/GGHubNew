7e54deee3412a20ecf15f1fc41f17666bdb294cc
bbb907 <logtoken2@gmail.com>
2026-08-17 04:08:59 +0100

    Update loopUtil.lua

--- loopUtil.lua
+++ loopUtil.lua
@@ -1,6 +1,7 @@
 local loops = {}
 
 local createdLoops = {}
+local events = {}
 
 function loops:CreateLoop(name, callback, interval)
 	if createdLoops[name] ~= nil then
@@ -34,3 +35,26 @@ function loops:UnbindLoop(name)
 		warn("Loop with name "..name.." does not exist!")
 	end
 end
+
+function loops:AddRobloxEvent(a: RBXScriptConnection)
+	table.insert(events,a)
+end
+
+function loops:UpdateRobloxEvents()
+	for _,v: RBXScriptConnection in pairs(events) do
+		if not v.Connected then
+			table.remove(events,table.find(events,v))
+		end
+	end
+end
+
+function loops:UnbindAll()
+	for i,v in pairs(createdLoops) do
+		v:Unbind()
+	end
+	for i,v in pairs(events) do
+		v:Disconnect()
+	end
+end
+
+return loops
