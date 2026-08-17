33766ee02c19d220bff60d3ba6784a21bfa003dd
bbb907 <logtoken2@gmail.com>
2026-08-17 17:57:56 +0100

    Create savingUtil.lua

---
+++ Util/savingUtil.lua
@@ -0,0 +1,92 @@
+local saving = {}
+local data_profiles = {}
+local httpService = game:GetService("HttpService")
+
+local folderPath
+
+function saving:init(fp)
+	
+	if not writefile or not listfiles or not readfile or not makefolder or not isfolder then
+		warn("SavingUtil: Your bad executor has like 0 unc, get a new one.")
+		return
+	end
+	
+	folderPath = fp
+	
+	if isfolder(folderPath) then
+		saving:LoadAllProfiles()
+		return
+	end
+	
+	makefolder(folderPath)
+	
+end
+
+function getFuncs(name)
+	local funcs = {}
+	local saving = false
+
+	function funcs:Get()
+		return data_profiles[name]
+	end
+
+	function funcs:Set(p)
+		data_profiles[name] = p
+	end
+
+	function funcs:Save()
+		if saving then return end
+		
+		saving = true
+		
+		writefile(folderPath.."/"..name,httpService:JSONEncode(data_profiles[name]))
+		
+		task.delay(2,function()
+			saving = false
+		end)
+	end
+
+	return funcs
+end
+
+function saving:CreateProfile(name)
+	
+	local profile = data_profiles[name]
+	
+	if profile then
+		warn("Profile already exists!")
+		return saving:GetProfile(name)
+	end
+	
+	data_profiles[name] = {}
+	
+	return getFuncs(name)
+	
+end
+
+function saving:GetProfile(name)
+	local profile = data_profiles[name]
+	
+	if not profile then
+		warn("Profile does not exist!")
+		return
+	end
+
+	data_profiles[name] = {}
+
+	return getFuncs(name)
+end
+
+function saving:LoadProfile(path)
+	local s,e = pcall(function()
+		table.insert(data_profiles,httpService:JSONDecode(readfile(path)))
+	end)
+end
+
+function saving:LoadAllProfiles()
+	for _,v in pairs(listfiles(folderPath)) do
+		saving:LoadProfile(v)
+	end
+end
+
+return saving
