659efcf5667387754bbc8288d37947b363f220f0
bbb907 <logtoken2@gmail.com>
2026-08-21 18:23:54 +0100

    Update MapSerialiser.lua

--- Extra/MapSerialiser.lua
+++ Extra/MapSerialiser.lua
@@ -2,7 +2,6 @@ local HttpService = game:GetService("HttpService")
 Serialization = {};
 
 -- Import services
-local Tool = loadstring(game:HttpGet("https://raw.githubusercontent.com/bbb907/GGHubNew/refs/heads/main/Util/btoolsUtil.lua"))().fetchBtools()
 local Support = loadstring(game:HttpGet("https://raw.githubusercontent.com/bbb907/GGHubNew/refs/heads/main/Extra/SerialiserHelper.lua"))()
 Support.ImportServices();
 
